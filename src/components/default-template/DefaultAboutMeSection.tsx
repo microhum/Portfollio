b1ef174fb39c12fb64d844bb437361a9d82405ab
microhum <Guntee12123@gmail.com>
2023-11-08 14:44:56 +0700

    extend profile description

--- src/components/default-template/DefaultAboutMeSection.tsx
+++ src/components/default-template/DefaultAboutMeSection.tsx
@@ -8,13 +8,13 @@ import { Section } from "shared/Section";
 export const DefaultAboutMeSection = () => {
   return (
     <Section id="about-me" headingText="Guntee Doungmanee">
-      <div className="animate-hidden flex flex-col w-full gap-[30px] max-w-[450px]">
+      <div className="animate-hidden flex flex-col w-full gap-[30px] max-w-[800px]">
         <Typography tag="p" weight="semibold" className="text-primary text-xl sm:text-2xl">
           {"Student"}
         </Typography>
         <Typography tag="p" className="text-color2 text-l sm:text-xl whitespace-pre-line">
           {
-            "My journey has started since 2021 ~1 year \n Starting programming so lately which mean more effort and attention giving into it !"
+            "My journey has started since 2021 (around 2 years ago) \n Starting programming so lately which mean more effort and attention giving into it !"
           }
         </Typography>
         <div className="flex gap-[12px]">
